f6ff00d024e6fa479fd086d80e9c5f58b04d414b
Lauriane Samuel <l7uri4ne@gmail.com>
2022-04-01 11:38:08 +0200

    making footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,14 @@
 
-        <footer>Test footer ?</footer>
+        <footer>
+            <div class="footerContent">
+                <div>
+                    <p>API Website: <a href="https://www.thecocktaildb.com/" target="_blank">https://www.thecocktaildb.com/</a></p>
+                    <p>Copyright ©2022 || Lauriane Samuel</p>
+                </div>
+                <div>
+                    <a href="/contact">Contact me</a>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
